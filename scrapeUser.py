e0b277d8d8b28c5ab3f058ca69abfd8f2b3714bb
Jacky Liang <jackyliang@berkeley.edu>
2016-02-21 00:12:20 -0600

    added page parser

--- scrapeUser.py
+++ scrapeUser.py
@@ -1,9 +1,13 @@
 from re import search, compile
-import datetime
 
 class PageParser:
-	r_likes = compile("Creation Date</key>[\S\s]*?<date>(.*?)</date>") #Search likes
-	r_date = compile("Entry Text</key>[\S\s].*?<string>([\S\s]*?)</string>") #Search Date
-	r_url = compile("UUID</key>[\S\s]*?<string>(.*?)</string>")	#Search og:image
-	r_nurl = compile("UUID</key>[\S\s]*?<string>(.*?)</string>")
+    r_likes = compile("\"likes\":{\"count\":(\d\d),\"") #Search likes
+    r_date = compile(",\"date\":(.*?),") #Search Date
+    r_url = compile("og:image.+?(https://.+?.jpg)")	#Search og:image
 
+    @staticmethod
+    def get_info_from_post(raw):
+        likes = int(search(PageParser.r_likes, raw).group(1))
+        date = float(search(PageParser.r_date, raw).group(1))
+        img_url = search(PageParser.r_url, raw).group(1)
+        print likes, date, img_url
